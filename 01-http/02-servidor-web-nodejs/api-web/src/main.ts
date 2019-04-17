a2dc7d17042c949aa347f11ba91db5ad7bc38c25
cabp1996 <41761406+cabp1996@users.noreply.github.com>
2019-04-17 15:58:07 -0500

    Commit Parametros Ruta, Cuerpo, Cookies

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+var cookieParser= require('cookie-parser') //forma de importar en JS
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   await app.listen(3003);
 }
 bootstrap();
